26850a04550f7f257a0189fb0ee4a13d4a578f10
Josival <jcs28@aluno.ifal.edu.br>
2022-06-20 13:09:16 +0000

    add api rest to subtraction (#7)

--- lib/SubHandler.java
+++ lib/SubHandler.java
@@ -1,26 +1,26 @@
 import static java.net.HttpURLConnection.HTTP_OK;
-
 import java.io.IOException;
 import java.io.OutputStream;
-
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class IndexHandler implements HttpHandler {
+public class MultiHandler implements HttpHandler {
 
-    public static final String PATH = "/";
+    public static final String PATH = "/sub";
 
     @Override
     public void handle(HttpExchange conn) throws IOException {
-        byte[] hello = "Seja bem-vindo(a) a Calc API! Temos os seguintes endpoints: /multi, /div, /sub".getBytes();
-
+        String[] partes = conn.getRequestURI().getPath().split("/");
+        String parametro1 = partes[2];
+        String parametro2 = partes[3];
+        byte[] result = calculateResponse(parametro1, parametro2);
         try {
-            conn.sendResponseHeaders(HTTP_OK, hello.length);
+            conn.sendResponseHeaders(HTTP_OK, result.length);
             Headers headers = conn.getResponseHeaders();
             headers.add("Content-Type", "text/html; charset=UTF-8");
             try (OutputStream out = conn.getResponseBody()) {
-                out.write(hello);
+                out.write(result);
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
@@ -29,5 +29,12 @@ public class IndexHandler implements HttpHandler {
         } finally {
             conn.close();
         }
+    }
+
+    byte[] calculateResponse(String parametro1, String parametro2) {
+        double n1 = Double.parseDouble(parametro1);
+        double n2 = Double.parseDouble(parametro2);
+        double sub = n1 - n2;
+        return Double.toString(sub).getBytes();
     }
 }
