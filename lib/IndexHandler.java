70b03c7c6f17809fe7c57c96fe66c899a9b76b9c
Josival <103062552+Josival@users.noreply.github.com>
2022-06-20 10:06:33 -0300

    Update IndexHandler.java

--- lib/IndexHandler.java
+++ lib/IndexHandler.java
@@ -13,7 +13,7 @@ public class IndexHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange conn) throws IOException {
-        byte[] hello = "Seja bem-vindo(a) a Calc API! Temos os seguintes endpoints: /multi, /div, /soma".getBytes();
+        byte[] hello = "Seja bem-vindo(a) a Calc API! Temos os seguintes endpoints: /multi, /div, /sub".getBytes();
 
         try {
             conn.sendResponseHeaders(HTTP_OK, hello.length);
