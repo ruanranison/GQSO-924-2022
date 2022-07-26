0f23a48a1eab659f48869b56dc628e48d5a8f741
ruanranison <rrcs1@aluno.ifal.edu.br>
2022-07-26 16:16:44 +0000

    added tests unit of multi

---
+++ myapp/src/test/java/app/MultiTest.java
@@ -0,0 +1,37 @@
+package com.danielfireman.ifal.calcapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
+
+public class MultiTest {
+    @Test
+    public void multi() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/multi/2/2", rsp -> {
+            assertEquals(4.0, rsp.value());
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void multi_operador1String() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/multi/aa", rsp -> {});
+        });
+    }
+
+    @Test
+    public void multi_operador2String() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/multi/1/aa", rsp -> {});
+        });
+    }
+}
