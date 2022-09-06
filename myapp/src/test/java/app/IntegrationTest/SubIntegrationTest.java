01a7e30728ca24d9febca2086099e06f81a1ea34
Josival <jcs28@aluno.ifal.edu.br>
2022-09-06 17:02:59 +0000

    Remoção dos espaços pedidos

--- myapp/src/test/java/app/IntegrationTest/SubIntegrationTest.java
+++ myapp/src/test/java/app/IntegrationTest/SubIntegrationTest.java
@@ -1,16 +1,12 @@
 package app.IntegrationTest;
-
 import io.jooby.JoobyTest;
 import io.jooby.StatusCode;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.jupiter.api.Test;
-
 import app.App;
-
 import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JoobyTest(App.class)
